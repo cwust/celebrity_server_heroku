12679fff78e645c44d4473119ace31d36637621c
Charles Wust <charles.wust@gmail.com>
2017-11-22 23:21:45 -0200

    accept CORS

--- new_server.js
+++ new_server.js
@@ -2,16 +2,36 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-const path = require('path');
 const Case = require('case');
+var session = require('express-session')
+var MemoryStore = require('memorystore')(session)
+var cors = require('cors')
 
 const CelebrityEngine = require('./celebrity-engine');
 
 const PORT = process.env.PORT || 3000;
 
-const server = express();
+const app = express();
 
-console.log('CelebrityEngine', CelebrityEngine);
+app.use(session({
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  saveUninitialized: true,
+  secret: 'cwustcelebrity',
+  secure: false
+}));
+
+
+const corsOptions = {
+  origin: 'http://localhost:8100',
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+
 
 for (let method in CelebrityEngine) {
   let route = '/' + Case.kebab(method);
@@ -25,15 +45,28 @@ for (let method in CelebrityEngine) {
       result: {error: err.message};
       console.error('Error', err);
     }
-    console.log('result', result);
 
-    res.send(JSON.stringify(result));
+    console.log(req.session);
+    console.log('req.session.data before', req.session.data);
+    
+    req.session.data = 1;
+    req.session.save(() => {
+      console.log('req.session.data after ', req.session.data);
+      res.send(JSON.stringify(result));
+    });
+
+    if (result.playerInfo) {
+      req.session.playerInfo = result.playerInfo;
+      req.session.save();
+    }
+
+    
   };
 
-  server
+  app
     .post(route, handler)
     .get(route, handler);
 }
 
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
